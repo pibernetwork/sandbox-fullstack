53651105079398c79f0fd6b18e408e1c8dd3383a
Mauricio Piber <mauriciopiber@gmail.com>
2023-11-14 18:22:49 -0300

    feat(server): add vercel

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,58 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import gql from 'graphql-tag';
+import http from 'node:http';
+import 'reflect-metadata';
+
 import DataServices from './containers/data-services.js';
 import container from './containers/inversify.config.js';
 
+dotenv.config();
+
 const { myClass } = new DataServices(container);
 
-/* eslint-disable no-console */
-console.log(myClass.myValue);
-/* esline-enable no-console */
+const typeDefs = gql.default`
+  type Query {
+    hello: String
+  }
+`;
 
-interface TestA {
-  value: string;
-}
+const resolvers = {
+  Query: {
+    hello: () => myClass.myValue,
+  },
+};
 
-const a: TestA = { value: '1234' };
+const { json } = bodyParser;
 
-/* eslint-disable no-console */
-console.log(a);
-/* esline-enable no-console */
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  introspection: true,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  resolvers,
+  typeDefs,
+});
+
+await server.start();
+
+app.get('/', (_, response) => {
+  response.status(301).redirect('/graphql');
+});
+
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  json(),
+  expressMiddleware(server),
+);
+
+export default httpServer;
