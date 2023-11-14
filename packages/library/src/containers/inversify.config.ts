005fe11d8932f29f508416042b2d1406c1d40930
Mauricio Piber <mauriciopiber@gmail.com>
2023-11-14 12:47:09 -0300

    feat(library): added inversify container

---
+++ packages/library/src/containers/inversify.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable max-lines */
+import { Container } from 'inversify';
+
+import MyClass from '../libs/my-class.js';
+import { TYPES } from './types.js';
+
+const container = new Container();
+
+// config
+container.bind<MyClass>(TYPES.MyClass).to(MyClass).inSingletonScope();
+
+export default container;
--- packages/library/src/libs/my-class.ts
+++ packages/library/src/libs/my-class.ts
@@ -1,3 +1,6 @@
+import { injectable } from 'inversify';
+
+@injectable()
 class MyClass {
   myValue = 15;
 }
