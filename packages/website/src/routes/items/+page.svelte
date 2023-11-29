bd978781b13805d71e40563f6a0c30e9fdcae95b
Mauricio Piber <mauriciopiber@gmail.com>
2023-11-29 15:34:35 -0300

    feat(website): call graphql inline for items list

--- packages/website/src/routes/items/+page.svelte
+++ packages/website/src/routes/items/+page.svelte
@@ -1,7 +1,42 @@
 <script lang="ts">
-  import Table from '$lib/items/table.svelte';
+  import { graphql } from '$houdini';
+
+  $: store = graphql(`
+    query GetItems @load @cache(policy: NetworkOnly) {
+      itemsConnection(page: 1, limit: 200, sortBy: "name", sortOrder: "desc") {
+        nodes @list(name: "All_Items") {
+          _id
+          name
+        }
+        pageInfo {
+          totalPages
+          totalNodes
+        }
+      }
+    }
+  `);
 </script>
 
 <h1>Items</h1>
 <a href="/items/add">Add</a>
-<Table />
+
+<p>List</p>
+<div class="p-2">
+  {#if $store?.data?.itemsConnection.nodes}
+    <div class="grid grid-cols-1 gap-2">
+      {#each $store?.data?.itemsConnection.nodes as item}
+        {#if item}
+          <div class="flex">
+            <div>{item.name}</div>
+            <div><a class="m-2" href={`/items/edit/${item._id}`}>Edit</a></div>
+            <div><a class="m-2" href={`/items/del/${item._id}`}>Del</a></div>
+            <div><a class="m-2" href={`/items/view/${item._id}`}>View</a></div>
+          </div>
+        {/if}
+      {/each}
+    </div>
+    <div>
+      Page {$store?.data?.itemsConnection.pageInfo?.totalNodes}
+    </div>
+  {/if}
+</div>
