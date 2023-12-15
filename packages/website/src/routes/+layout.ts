951699d32c9a6c01feb5fae9cd6857c29666ded0
Mauricio Piber <mauriciopiber@gmail.com>
2023-12-15 12:16:03 -0300

    feat(website): removed svelte-kit

--- packages/website/src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-export const ssr = false;
--- packages/website/src/routes/items/del/[id]/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ params }) => {
-  const { id } = params;
-
-  return { id };
-};
