a5dd95bb080f6d8458fcc2ec11a3aa547bd4f3aa
Mauricio Piber <mauriciopiber@gmail.com>
2023-12-15 12:34:17 -0300

    feat(website): added nextjs

---
+++ packages/website/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <h1>Back to NextJS</h1>
+      <p>
+        I have pass my most happy days of life working with NextJS. Genuinely
+        happy. Let&apos;s live this hapinnes again
+      </p>
+    </main>
+  );
+}
